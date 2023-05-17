0fb1305a106f6e3bcffcc48c124dae9b92fdac9a
21Horza <im.shoo@yandex.ru>
2023-05-17 21:52:35 +0800

    finish setup test env, add mock data to db

---
+++ app/application/tests/conftest.py
@@ -0,0 +1,56 @@
+import datetime
+import json
+import pytest
+import asyncio
+from sqlalchemy import insert
+from app.domain.shared.config.config import settings
+from app.infrastructure.database.database import Base, engine, async_session_maker
+from app.domain.entities.hotels.model.hotel_model import Hotels
+from app.domain.entities.users.model.user_model import Users
+from app.domain.entities.rooms.model.room_model import Rooms
+from app.domain.entities.bookings.model.booking_model import Bookings
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_db():
+    assert settings.MODE == "TEST"
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_data(model: str):
+        with open(f"app/application/tests/mock_data/{model}.json", encoding="utf-8") as file:
+            print(f"app/application/tests/mock_data/{model}.json")
+            return json.load(file)
+        
+    hotels = open_mock_data("hotels")
+    rooms = open_mock_data("rooms")
+    users = open_mock_data("users")
+    bookings = open_mock_data("bookings")
+
+    for booking in bookings:
+        booking["date_from"] = datetime.datetime.strptime(booking["date_from"], "%Y-%m-%d")
+        booking["date_to"] = datetime.datetime.strptime(booking["date_to"], "%Y-%m-%d")
+
+
+    async with async_session_maker() as session:
+        add_hotels = insert(Hotels).values(hotels)
+        add_rooms = insert(Rooms).values(rooms)
+        add_users = insert(Users).values(users)
+        add_bookings = insert(Bookings).values(bookings)
+
+        await session.execute(add_hotels)
+        await session.execute(add_rooms)
+        await session.execute(add_users)
+        await session.execute(add_bookings)
+
+        await session.commit()
+
+# Official docs pytest-asyncio
+# Create new event loop to run all tests
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
